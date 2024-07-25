98b30d5eefdc42a78ca9a6b12eeb3c94f506783d
mj <mikulej7@gmail.com>
2024-07-25 20:33:27 +0200

    select country

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
     df = pd.read_csv("bigmac.csv")
 
     #get names of countries
-    print("List of countries: ", df['name'].unique())
+    country_list = df['name'].unique()
+    print("List of countries: ", country_list)
 
     #filter rows between 2005-2015
     mask = (df['date'] > '2005-1-01') & (df['date'] <= '2015-1-01')
@@ -30,10 +31,18 @@ def main():
     print(price_means.min())
     print(price_means.max())
     #create business dashboard
-    #     st.write("""
-# # My first app
-# Hello *world!*
-#              """)
+    st.set_page_config(page_title="Business DashBoard",
+                       page_icon=":bar_chart:",
+                       layout="wide",
+                       initial_sidebar_state="expanded")
+    
+    st.sidebar.header("Dashboard")
+    #st.sidebar.subheader("Choose country")
+    selected_country = st.sidebar.selectbox('Country',country_list)
+    st.write("""
+# My first app
+Hello *world!*
+             """)
 
 
 if __name__ == "__main__":
